9e7e720ed7eadd2c380728cfe71be912467b2bad
zhengfw <zfw55zfw@sina.com>
2019-05-31 15:54:40 +0800

    push

--- src/show/designMode/abstractFactory/MailFactory.java
+++ src/show/designMode/abstractFactory/MailFactory.java
@@ -1,10 +1,6 @@
 package show.designMode.abstractFactory;
 
-/**
- * @author zhengfawei
- * @create 2019-04-29 上午10:07
- * @desc
- **/
+
 public class MailFactory implements Provider {
 
     @Override
