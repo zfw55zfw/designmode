3091e75cb61351446449a90af626352ab86d45cc
zhengfw <zfw55zfw@sina.com>
2019-05-31 15:49:55 +0800

    push

--- src/show/designMode/classAdapter/Test.java
+++ src/show/designMode/classAdapter/Test.java
@@ -12,7 +12,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println(a);
-        URI
 //        Targetable target = new Adapter();
 //        target.method1();
 //        target.method2();
